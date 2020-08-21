c4242bedbc309cda4c33abb722e404e294916c47
Kiernan Roche <self@kiernanro.ch>
2020-08-21 17:58:24 -0400

    Add PicoComments.php and comments.twig, update README.md

---
+++ PicoComments.php
@@ -0,0 +1,204 @@
+<?php
+
+/**
+* Plugin implementing comments in Pico
+* @author kroche
+* @license http://opensource.org/licenses/MIT
+*/
+
+class PicoComments extends AbstractPicoPlugin
+{
+    protected $enabled = true;                      // whether this plugin is enabled by default sitewide
+    protected $content_path = __DIR__ . '/content'; // content storage path. by default this is inside the plugin directory itself so that everything is self-contained
+    protected $headers;                             // current page headers ("meta", "frontmatter", NOT HTML headers)
+    protected $id;                                  // current page URL ("id")
+    protected $num_comments = 0;                    // number of comments on this page
+
+    private function createComment($author, $content, $reply_guid) {
+        $guid = bin2hex(random_bytes(16));      // create a GUID for this comment
+        $date = time();                         // get the current time
+        $ip = $_SERVER['REMOTE_ADDR'];          // get the ip address the comment is posted from
+        
+        // user input sanitization
+        $author = strlen($author) != 0 ? filter_var($author, FILTER_SANITIZE_STRING) : null;
+        $content = strlen($content) != 0 ? htmlspecialchars($content, ENT_QUOTES, ini_get("default_charset")) : null;
+
+        // fail if author or content is empty, or if content is larger than the comment size limit
+        if (!isset($author) || !isset($content) || strlen($content) > $this->getPluginConfig("comment_size_limit")) {
+            return false;
+        }
+        
+        // path where the current page's comments are stored
+        $path = $this->content_path . "/" . $this->id;
+
+        if (!file_exists($path)) {
+            mkdir($path, 0755, true);
+        } else if (isset($reply_guid)) {
+            $reply_exists = false;          // whether $reply_guid corresponds to a real comment. false by default
+            $dir = glob($path . "/*.md"); 
+            foreach ($dir as $file) {       // check if the comment pointed to by reply_guid exists, fail if not
+                if (array_slice(explode("/", explode(".", $file)[0]), -1, 1)[0] == $reply_guid) {
+                   $reply_exists = true;
+                   break;
+                }
+            }
+            if (!$reply_exists) {   // if the comment that reply_guid points to can't be found,
+                return false;       // don't create the comment
+            }
+        }
+
+        // build the comment file
+        // this is hacky
+        $file_contents = "---\n";
+        $file_contents .= "guid: " . $guid . "\n";
+        if (isset($reply_guid)) { $file_contents .= "reply_guid: " . $reply_guid . "\n"; }
+        $file_contents .= "date: " . strval($date) . "\n";
+        $file_contents .= "ip: " . strval($ip) . "\n";
+        $file_contents .= "author: " . $author . "\n";
+        // if comment review is enabled, add header
+        if ($this->getPluginConfig("comment_review")) { $file_contents .= "pending: true\n"; }
+        $file_contents .= "---\n";
+        $file_contents .= $content;
+
+        $handle = fopen($path . "/" . $guid . ".md", "w");
+        if (!fwrite($handle, $file_contents)) { // if file writing fails for some reason
+            fclose($handle);
+            return false;
+        }
+        fclose($handle);
+        return true;
+    }
+
+    // return a nested array of hashes
+    // [{"author":"me", "content":"hello", "replies":[{"author":"him", "content":"hi there"}]}]
+    // replies to each comment are stored as a sub-array which can be recursed through in twig
+    private function getComments() {
+        $comments = []; // this is the dictionary where comment dictionaries will be stored
+
+        $dir = glob($this->content_path . "/" . $this->id . "/*.md");
+        // this loop reads comments from disk into memory as a dictionary
+        // for each file in the content page dir:
+        foreach ($dir as $file) {
+            // read in the file
+            try {
+                // IP address left out to prevent leaks to users. it's for administrative use only i.e. blocking spam
+                $headers = [
+                    "GUID" => 'guid',
+                    "Reply GUID" => 'reply_guid',
+                    "Author" => 'author',
+                    "Date" => 'date',
+                    "Pending Review" => 'pending'
+                ];
+                
+                // parse headers and content
+                $meta = $this->parseFileMeta(file_get_contents($file), $headers);
+                // Pico's parseFileContent function doesn't ignore frontmatter so I'm rolling my own here
+                $content = explode("---\n", file_get_contents($file))[2]; 
+            } catch (\Symfony\Component\Yaml\Exception\ParseException $e) {
+                error_log($e->getMessage());
+                // continue to next loop iteration, we don't want improperly parsed comments to exist in the dictionary
+                continue;
+            }
+            
+            // build the dictionary entry
+            if (!$meta['pending']) {  // if the comment is not pending review or has been approved
+                $comment = [
+                    'content' => $content
+                ];
+                
+                foreach($meta as $key => $value) {
+                    if (strlen($meta[$key]) > 0) {
+                        $comment[$key] = $value;
+                    }
+                }
+                
+                // insert into dict
+                if (strlen($meta['reply_guid']) > 0) {
+                    $comments[$meta['reply_guid']][] = $comment;
+                } else {
+                    $comments[""][] = $comment;
+                }
+                
+                // increment the comments counter
+                $this->num_comments++;
+            }
+        }
+        
+        // this recursive function builds and sorts the child-pointer tree
+        function insert_replies(&$array, &$comments) {
+            foreach($array as &$comment) {
+                // if this comment has children,
+                if (isset($comments[$comment['guid']])) {
+                    // recurse first so children are fully populated with their own descendants
+                    insert_replies($comments[$comment['guid']], $comments);
+                    // insert children into this comment's replies array
+                    $comment['replies'] = $comments[$comment['guid']];
+                    // sort the replies
+                    usort($comment['replies'], function($a, $b) { return $b['date'] <=> $a['date']; });
+                    // remove descendants from the top level
+                    unset($comments[$comment['guid']]);
+                }
+            }
+            
+            // any replies that exist have been inserted
+            return;
+        }
+
+        // build the tree
+        insert_replies($comments[""], $comments);
+
+        // remove the extra array wrapper around the top level
+        $comments = $comments[""];
+        // sort the top level
+        usort($comments, function($a, $b) { return $b['date'] <=> $a['date']; });
+
+        return $comments;
+    }
+
+    public function onMetaParsed(array &$headers) {
+        $this->headers = $headers;  // store the headers of the current page
+    }
+
+    public function onPageRendering(&$templateName, array &$twigVariables) {
+        if (!isset($this->headers['comments'])) {   // in this case, we assume that this page does not support comments 
+            return;                                 // do nothing
+        }
+
+        $this->id = $this->pico->getCurrentPage()['id'];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {        // if a comment is submitted
+            if ($this->headers['comments'] != "enabled") {  // if comment submission is disabled on this page, do nothing
+                $twigVariables['comments_message'] = "Comment submission is disabled on this page"; // display error message and status 1
+                $twigVariables['comments_message_status'] = 1;
+                return;
+            }
+            
+            // check if antispam honeypot is filled out
+            if (isset($_POST['website']) && strlen($_POST['website']) > 0) {
+                return;
+            }
+
+            if (!isset($_POST['comment_author']) || !isset($_POST['comment_content'])) {    // if we are missing comment author or content
+                $twigVariables['comments_message'] = "Please fill out all required fields.";// display error message and status 1
+                $twigVariables['comments_message_status'] = 1;
+            }
+            
+            $reply_guid = isset($_POST['comment_replyguid']) ? $_POST['comment_replyguid'] : null;  // set reply_guid to null if it comment_replyguid is not included (i.e. if this comment is not a reply)
+            
+            if ($this->createComment($_POST['comment_author'], $_POST['comment_content'], $reply_guid)) {         // if CreateComment is successful
+                $twigVariables['comments_message'] = "Comment submitted";       // display success message and status 0
+                $twigVariables['comments_message_status'] = 0;
+            } else {                                                            // if createComment fails for some reason
+                $twigVariables['comments_message'] = "Comment not submitted";   // display fail message and status 1
+                $twigVariables['comments_message_status'] = 1;
+            }
+
+            $twigVariables['comments'] = $this->getComments() ?: "Server error";// display comments or fail, since we want to display comments after a new comment has been submitted to show the user their new comment
+            $twigVariables['comments_number'] = $this->num_comments ?: "0";
+
+        } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {                      // if this is a GET request
+            $twigVariables['comments'] = $this->getComments() ?: "Server error";// display comments or fail
+            $twigVariables['comments_number'] = $this->num_comments ?: "0";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,123 @@
-# pico-comments
-A comments plugin for Pico CMS
+#pico-comments
+
+This plugin implements comments for Pico CMS.
+
+I wrote this for my own personal use, but felt that because a comments plugin for Pico did not exist yet that others might get some use out of it. 
+
+Features
+---
+- Comments stored in a similar manner to pages
+- Comment replies
+- Comment review
+- Comment size limits
+- Antispam using a honeypot form field
+- IP address logging
+- Theoretically scalable
+
+Usage and configuration
+---
+To use this plugin, you need to:
+1. Add PicoComments.php to your Pico plugins directory
+2. Configure and enable it in your config.yml
+3. Add comment submission forms and rendering macros to your theme
+
+The plugin is configured in the site config.yml like so:
+```
+PicoComments:
+    # whether the comments plugin is enabled. comment data will not be available to Twig if this is false
+    enabled: true
+    # maximum character size of comments. comments larger than this will be rejected
+    comment_size_limit: 40000
+    # whether comments must be approved before displaying them to other users
+    comment_review: false
+```
+
+Comment submission can be enabled/disabled for each page in that page's frontmatter:
+```
+---
+comments: true
+---
+```
+
+If ```comments``` is set to ```false```, existing comments on that page will still be available to Twig for rendering, but no new comments will be accepted.
+
+Comment data and comment submission success/error messages are exposed as Twig variables. Please see comments.twig for an example comment submission form and comment rendering code.
+
+Comments that are submitted while ```comment_review``` is enabled in the site config are not exposed to Twig. To approve a comment that is under review, open it with a text editor and change the "pending" field from "true" to "false". To deny it, leave "pending" as "true" or delete the comment file from disk.
+
+Technical details
+---
+Comments are created by submitting a POST request to the URL of a page with comments enabled. The plugin looks for these parameters:
+- comment_author
+- comment_content
+- comment_replyguid must be included if the comment is a reply to another comment
+
+All other comment properties (guid, date of posting, ip address, pending review) are generated on the server. 
+
+Comment content and author names are sanitized before being saved. Comment reply GUIDs are validated before the comment is saved, and if the comment pointed to by the reply GUID does not exist, the comment will not be accepted.
+
+Comments are stored as files in the plugin's ```content``` directory in a manner very similar to Pico's own page structure. The structure of a comment file is as follows:
+```
+---
+guid
+guid of comment being replied to, if this comment is in reply to another comment
+date posted, in UNIX time
+ip address of author
+author name
+pending status
+---
+content
+```
+
+Files are named using the comment guid followed by a ```.md``` extension, though I chose not to render the comment content as Markdown to avoid cross-site scripting issues.
+
+Comment storage design
+---
+I chose to store comments on disk as a parent-pointer tree, because a child-pointer representation would require comments to maintain a list of children which is updated each time the comment is replied to, which is not necessary with a parent-pointer tree. However, a parent-pointer tree is much harder to recurse from the top down, which is what we want when rendering the comments; creating a parent comment as a div and recursing downward to create its children inside of it is much simpler than recursing upward and storing child comments in memory until we reach the top of the tree and then rendering downwards somehow (this is the sort of thing that should **not** be done in Twig). We achieve the best of both schemes by storing comments as parent-pointer and exposing them to Twig as child-pointer, making it easy for Twig to render replies recursively without complex logic. Using both tree types necessitates some means of transforming one to the other, which is the core logic of this plugin.
+
+First, comments are read from disk into memory. Each comment is stored as a dictionary of its frontmatter (meta) keys to the values corresponding to them, plus a "content" key that contains its content:
+```
+Array
+(
+    [guid] => ce7b48f8ce53dff6af8a3f4b5b9261d6
+    [reply_guid] => d3b964839536a5fdb1006e6f9d9140e2
+    [date] => 2020-08-20 15:25:27
+    [ip] => 127.0.0.1
+    [author] => test5
+    [time] => 1597951527
+    [date_formatted] => August 08, 2020 15:25
+    [content] => Hello, world!
+)
+```
+
+A PHP dictionary is used to store comments: comment GUIDs as keys, arrays which contain children of that comment as values. If a comment has a parent, it's placed in the array corresponding to that parent's GUID. If it doesn't have a parent (top level comment), it's placed in an array with a null key ([""]).
+
+Once we have established the comment structure in memory, we transform it. The transformation algorithm works like this:
+```
+Given an array of comments (starting with the array of top level comments corresponding to the null key), for each comment in the array:
+    If this comment's GUID is a key in the dictionary (it has children):
+        Recurse into the array corresponding to that GUID (recursion happens first so that children are fully populated with their own descendants before moving them)
+        Set this comment's replies array equal to that array of children, making a copy inside this comment
+        Sort this comment's replies array by date
+        Remove array of children from the dictionary now that it is contained within this comment
+```
+
+Once the tree is transformed, we expose it to Twig as the ```comments``` variable, which can be recursed using a Twig macro to render it. We can access the total number of comments in Twig using the ```comments_number``` variable.
+
+The use of a dictionary to build the child-pointer tree eliminates any need for searching, so this plugin should scale to large numbers of comments, but I haven't tested it with anything more than a few dozen comments.
+
+Issues, assumptions, future ideas
+---
+- Comments are stored in a hierarchy that mirrors the structure of the site. If the site structure changes, the comments structure needs to be changed to match. This could be addressed by changing the design to store the page ID within each comment and all comments in a single directory, but I decided against that, instead keeping comments for each page in a directory corresponding to that page, which improves performance by limiting the tree-building loops only to the comments on the current page.
+- There is no captcha, but the current antispam solution should suffice unless someone writes a bot specifically to ignore the honeypot form field. IP addresses are stored, so IP-based antispam would be straightforward to implement.
+- Users cannot delete comments, because no user authentication is implemented. A simple approach might be to use PHP sessions to allow users to delete their comments as long as their session is valid, but not after their session cookie expires and they are no longer identifiable.
+- The plugin responds to a POST request directly with a newly rendered page rather than issuing a redirect to the same page. This means that if the user refreshes the page after submitting a comment, they may submit a duplicate comment. I chose to do this because submission messages (success/error, etc.) are exposed as Twig variables, which means they will not survive a redirect since they exist on a per-request basis. Once again, sessions could address this.
+- There is some code duplication between the print_comments macro and the top-level submission form. This could be resolved with another macro.
+
+Contributing
+---
+I welcome suggestions, comments, and code. 
+- If you feel your contribution would improve this plugin for the general use case, please submit a pull request. 
+- If you find a bug, open an issue and describe what's happening. Please include verbose PHP and web server logs and screenshots of the issue, where applicable.
+
+I don't have a lot of time to continue development of this plugin except for my own purposes. I can't guarantee that I will fix issues or implement new features promptly, or at all. If I do, and if I believe such improvements would be useful to users, I will release them.
